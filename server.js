ebcdab5355341f9b72a8ebb52d35bcc708128c60
Promise Fru <promisefru1@gmail.com>
2021-02-23 12:34:41 +0100

    server setup done

---
+++ server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+
+// initialize express
+var app = express();
+
+// initialize cors
+var corsOption = {
+    origin: 'http://localhost:3000'
+};
+app.use(cors(corsOption));
+
+// initialze bodyParser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// routes
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome, it's working"
+    });
+})
+
+// set port, listen for request
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
