ab1d03e97fca845ae6c979fc8df0572cdb750926
Promise Fru <promisefru1@gmail.com>
2021-02-23 13:35:53 +0100

    database config done

---
+++ config/db.config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+(async () => {
+    var connection = await mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.OWNER,
+        password: process.env.PASSWORD
+    });
+
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DATABASE}\``)
+    console.log("DATABASE IS READY");
+})();
+
+module.exports = {
+    HOST: process.env.HOST,
+    USER: process.env.OWNER,
+    PASSWORD: process.env.PASSWORD,
+    DB: process.env.DATABASE,
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+}
