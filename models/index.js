528427da5cafd5e4d330bd815ff930d20817c984
Promise Fru <promisefru1@gmail.com>
2021-02-24 11:46:29 +0100

    models index created

--- models/index.js
+++ models/index.js
@@ -4,9 +4,34 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
     config.DB,
     config.USER,
-    config.PASSWORD,
-    {
-        host:config.HOST,
+    config.PASSWORD, {
+        host: config.HOST,
         dialect: config.dialect
     }
-)
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = require('../models/user.model.js')(sequelize, Sequelize);
+db.role = require('../models/role.model.js')(sequelize, Sequelize);
+
+db.role.belongToMany(db.user, {
+    through: "user_roles",
+    foreignKey: "roleId",
+    otherKey: "userId"
+});
+
+db.user.belongToMany(db.role, {
+    through: "user_roles",
+    foreignKey: "userId",
+    otherKey: "roleId"
+});
+
+db.ROLES = ["user", "admin", "moderator"];
+
+// console.log(db)
+
+module.exports = db;
