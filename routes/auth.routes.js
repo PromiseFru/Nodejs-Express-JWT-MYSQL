9689192d6c095edffa85d329643bc7c13cfe1818
Promise Fru <promisefru1@gmail.com>
2021-03-01 06:49:23 +0100

    auth route created

---
+++ routes/auth.routes.js
@@ -0,0 +1,21 @@
+const {
+    verifySignup
+} = require('../middleware');
+const controllers = require("../controllers/auth.controller.js");
+
+module.exports = (app) => {
+    app.use((req, res, next) => {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept;"
+        );
+        next();
+    });
+
+    app.post("/api/auth/signup", [
+        verifySignup.checkDuplicateUsernameAndEmail,
+        verifySignup.checkRolesExisted
+    ], controllers.signup);
+
+    app.post("/api/auth/signin", controllers.signin);
+};
